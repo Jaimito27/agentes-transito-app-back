af2cb1dd56606e39f16069da8fda7ea33ec5a125
Jaimito27 <softjaimito27@gmail.com>
2025-06-02 00:58:08 -0500

    Se realiza la relación entre entidades y se corrigen detalles del aplication properties, dado que algunos errores impedian la correcta compilación. Se revisa la base de datos y todo tiene la configuración correcta

--- src/main/java/com/tiven/gestion_agentes_vias/AgenteTransito/AgenteTransito.java
+++ src/main/java/com/tiven/gestion_agentes_vias/AgenteTransito/AgenteTransito.java
@@ -1,8 +1,12 @@
 package com.tiven.gestion_agentes_vias.AgenteTransito;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import com.tiven.gestion_agentes_vias.HistoricoAsignacion.HistoricoAsignacion;
+import com.tiven.gestion_agentes_vias.Vias.Via;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -26,5 +30,13 @@ public class AgenteTransito {
     @Column(name = "codigo_secretaria_transito")
     private String codigoSecretariaTransito;
 
+    //Relacion con la tabla de via
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_via_actual")
+    private Via viaActual;
+
+    //Relacion bidireccional con Historico Asignación
+    @OneToMany(mappedBy = "agenteTransito", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<HistoricoAsignacion> historialAsignaciones = new ArrayList<>();
     
 }
--- src/main/java/com/tiven/gestion_agentes_vias/HistoricoAsignacion/HistoricoAsignacion.java
+++ src/main/java/com/tiven/gestion_agentes_vias/HistoricoAsignacion/HistoricoAsignacion.java
@@ -1,5 +1,7 @@
 package com.tiven.gestion_agentes_vias.HistoricoAsignacion;
 
+import com.tiven.gestion_agentes_vias.AgenteTransito.AgenteTransito;
+import com.tiven.gestion_agentes_vias.Vias.Via;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,5 +28,14 @@ public class HistoricoAsignacion {
     @Column(name = "usuario_asignador")
     private String usuarioAsignador;
 
+    //relacion con agente de transito
+    @ManyToOne(fetch = FetchType.LAZY) //carga peresoza
+    @JoinColumn(name = "id_agente_transito", nullable = false)
+    private AgenteTransito agenteTransito;
+
+    //Reación con via
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_via_asignada", nullable = false)
+    private Via viaAsignada;
 
 }
