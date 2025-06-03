ec4f922d9d51ba09dfeeaa6b3a36ab33ab2a7b8a
Jaimito27 <softjaimito27@gmail.com>
2025-06-03 01:16:30 -0500

    Se crea el servicio de 'Historico asignación', se implementa toda la lógica desarrollada al momento, y en su repositorio correspondiente se crean 2 metodos para facilitar busquedas y filtros; para buscar dentro de historicos agentes por su codigo y via deacuerdo a su clasificación y número. En el repositorio de vía se implementa el metodo personalizado para buscar una via segun su clasificación y número igualmente

--- src/main/java/com/tiven/gestion_agentes_vias/HistoricoAsignacion/HistoricoAsignacionRepository.java
+++ src/main/java/com/tiven/gestion_agentes_vias/HistoricoAsignacion/HistoricoAsignacionRepository.java
@@ -3,9 +3,14 @@ package com.tiven.gestion_agentes_vias.HistoricoAsignacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface HistoricoAsignacionRepository extends JpaRepository<HistoricoAsignacion, UUID> {
     //Metodos personalizados
+    List<HistoricoAsignacion> findByAgenteTransito_Codigo(String  codigoAgente);
+    List<HistoricoAsignacion> findByViaAsignada_EsCalleOCarreraAndViaAsignada_Numero(String esCalleOCarrera, Integer numero);
+
+    List<HistoricoAsignacion> findByAgenteTransito_CodigoAndViaAsignada_EsCalleOCarreraAndViaAsignada_Numero(String codigoAgente, String esCalleOCarrera, Integer numero);
 }
---
+++ src/main/java/com/tiven/gestion_agentes_vias/HistoricoAsignacion/HistoricoAsignacionService.java
@@ -0,0 +1,63 @@
+package com.tiven.gestion_agentes_vias.HistoricoAsignacion;
+
+import com.tiven.gestion_agentes_vias.AgenteTransito.AgenteTransitoRepository;
+import com.tiven.gestion_agentes_vias.Vias.ViaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class HistoricoAsignacionService {
+
+    @Autowired
+    private HistoricoAsignacionRepository historicoAsignacionRepository;
+
+    @Autowired
+    private AgenteTransitoRepository agenteTransitoRepository;
+
+    @Autowired
+    private ViaRepository viaRepository;
+
+    //mapear de entidad a dto
+    private HistoricoAsignacionDTO convertToDto(HistoricoAsignacion historico) {
+        HistoricoAsignacionDTO dto = new HistoricoAsignacionDTO();
+        dto.setId(historico.getId());
+        dto.setFechaAsignacion(historico.getFechaAsignacion());
+        dto.setUsuarioAsignador(historico.getUsuarioAsignador());
+
+        if (historico.getAgenteTransito() != null) {
+            dto.setIdAgente(historico.getAgenteTransito().getId());
+            dto.setCodigoAgente(historico.getAgenteTransito().getCodigo());
+            dto.setNombreAgente(historico.getAgenteTransito().getNombreCompleto());
+        }
+        if (historico.getViaAsignada() != null) {
+            dto.setIdVia(historico.getViaAsignada().getId());
+            dto.setTipoVia(historico.getViaAsignada().getTipo());
+            dto.setEsCalleOCarreraVia(historico.getViaAsignada().getEsCalleOcarrera());
+            dto.setNumeroVia(historico.getViaAsignada().getNumero());
+        }
+        return dto;
+
+    }
+    public List<HistoricoAsignacionDTO> getHistoricoByAgenteCodigo(String codigoAgente){
+        //validar que es el agente exista antes de bscar su hisitorico
+        if(agenteTransitoRepository.findByCodigo(codigoAgente).isEmpty()){
+            throw new ResourceNotFoundException("Agente con código "+codigoAgente+" no existe");
+        }
+        return historicoAsignacionRepository.findByAgenteTransito_Codigo(codigoAgente).stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<HistoricoAsignacionDTO> getHistoricoByVia(String esCalleOCarrera, Integer numero){
+        //Valida su la via existe antes de buscar su historico
+        if(viaRepository.findByEsCalleOCarreraAndNumero(esCalleOCarrera, numero).isEmpty()){
+            throw new ResourceNotFoundException("Vía "+esCalleOCarrera+" "+numero+" no encontrada");
+        }
+        return historicoAsignacionRepository.findByViaAsignada_EsCalleOCarreraAndViaAsignada_Numero(esCalleOCarrera, numero).stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+}
